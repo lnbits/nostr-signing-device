49e762e63e6be231e89aead96a28881492649a23
ben <ben@arc.wales>
2023-03-09 13:58:54 +0000

    Installer page looking goooood

--- installer/assets/js/templates.js
+++ installer/assets/js/templates.js
@@ -4,50 +4,9 @@ const templates = Object.freeze({
         fileName: "elements.json",
         value: stringifyNice([
             {
-                "name": "password",
+                "name": "privatekey",
                 "type": "ACInput",
-                "value": "ToTheMoon1",
-                "label": "Password for PoS AP WiFi",
-                "pattern": "",
-                "placeholder": "",
-                "style": "",
-                "apply": "text"
-            },
-            {
-                "name": "ssid",
-                "type": "ACInput",
-                "value": "",
-                "label": "WiFi SSID",
-                "pattern": "",
-                "placeholder": "",
-                "style": "",
-                "apply": "text"
-            },
-            {
-                "name": "wifipassword",
-                "type": "ACInput",
-                "value": "",
-                "label": "WiFi Password",
-                "pattern": "",
-                "placeholder": "",
-                "style": "",
-                "apply": "text"
-            },
-            {
-                "name": "socket",
-                "type": "ACInput",
-                "value": "",
-                "label": "Copy link from LNURLDevices extension in LNbits",
-                "pattern": "",
-                "placeholder": "",
-                "style": "",
-                "apply": "text"
-            },
-            {
-                "name": "lnurl",
-                "type": "ACInput",
-                "value": "true",
-                "label": "LNURL",
+                "label": "Nostr privatekey",
                 "pattern": "",
                 "placeholder": "",
                 "style": "",
