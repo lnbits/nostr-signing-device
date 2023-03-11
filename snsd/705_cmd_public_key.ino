1909491af952a097c539e7f02201adf1076b5d20
Vlad Stan <stan.v.vlad@gmail.com>
2023-03-12 00:34:28 +0200

    fix: use privatekey from file

--- snsd/705_cmd_public_key.ino
+++ snsd/705_cmd_public_key.ino
@@ -5,18 +5,18 @@
 */
 CommandResponse executePublicKey(String data) {
 
-  logInfo("executePublicKey!! 100");
-  String privateKeyHex = "4c89c4e0db793a7fac4881f7b6f209ab2c039d88ca64600537f3a94e855478ab";
+  FileData nostrFile = readFile(SPIFFS, global.nostrFileName.c_str()); // todo: check success
+  String privateKeyHex = nostrFile.data;
   int byteSize =  32;
   byte privateKeyBytes[byteSize];
   fromHex(privateKeyHex, privateKeyBytes, byteSize);
   PrivateKey privateKey(privateKeyBytes);
   PublicKey publicKey = privateKey.publicKey();
 
-  sendCommandOutput(COMMAND_PUBLIC_KEY, toHex(publicKey.point, sizeof(publicKey.point)));
+  String publicKeyHex = toHex(publicKey.point, sizeof(publicKey.point));
+  sendCommandOutput(COMMAND_PUBLIC_KEY, publicKeyHex);
 
-  logInfo("executeSharedSecret!! 110");
 
 
-  return {"Public Key", "done"};
+  return {"Public Key", publicKeyHex.substring(0, 16) + "..."};
 }
