d3480cf87bdb314c9e79872792f7a76a4b50721a
dni ⚡ <office@dnilabs.com>
2023-09-22 20:20:34 +0200

    [FEAT] add CI workflows and releases

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+command -v arduino-cli >/dev/null 2>&1 || { echo >&2 "arduino-cli not found. Aborting."; exit 1; }
+arduino-cli config --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json init
+arduino-cli core update-index
+arduino-cli core install esp32:esp32
+arduino-cli upgrade
+arduino-cli lib install ArduinoJson QRCode TFT_eSPI tiny-AES-c uBitcoin
+arduino-cli compile --build-path build --fqbn esp32:esp32:esp32 snsd/snsd.ino
