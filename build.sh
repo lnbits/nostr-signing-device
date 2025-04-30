85c0efd133c99c2ea1419246324162ec56a6468c
dni ⚡ <office@dnilabs.com>
2025-04-30 09:18:53 +0200

    fix more libs

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ arduino-cli core update-index
 arduino-cli upgrade
 # uBitcoin is broken on esp32 3.x.x
 arduino-cli core install esp32:esp32@2.0.17
-arduino-cli lib install ArduinoJson QRCode TFT_eSPI uBitcoin
+arduino-cli lib install ArduinoJson uBitcoin Base64 gmp-ino
 arduino-cli compile \
     --library ./libraries/TFT_eSPI \
     --library ./libraries/QRCode \
