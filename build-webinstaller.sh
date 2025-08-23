cb5205f7b2a6da2d4a32c7be977b11b2b9583e9b
dni ⚡ <office@dnilabs.com>
2025-08-23 17:54:42 +0200

    fixup!

--- build-webinstaller.sh
+++ build-webinstaller.sh
@@ -16,11 +16,11 @@ mkdir -p $INSTALLER_PATH
 for device in $(jq -r '.devices[]' ./hardware-installer/src/versions.json); do
     for version in $(jq -r '.versions[]' ./hardware-installer/src/versions.json); do
         mkdir -p $INSTALLER_PATH/$device/$version
-        wget $RELEASE/$version/$PROJECT_NAME-$version.ino.bin
-        wget $RELEASE/$version/$PROJECT_NAME-$version.ino.partitions.bin
-        wget $RELEASE/$version/$PROJECT_NAME-$version.ino.bootloader.bin
-        mv $PROJECT_NAME-$version.ino.bin $INSTALLER_PATH/$device/$version/main.bin
-        mv $PROJECT_NAME-$version.ino.partitions.bin $INSTALLER_PATH/$device/$version/partitions.bin
-        mv $PROJECT_NAME-$version.ino.bootloader.bin $INSTALLER_PATH/$device/$version/bootloader.bin
+        wget $RELEASE/$version/$PROJECT_NAME-$device.ino.bin
+        wget $RELEASE/$version/$PROJECT_NAME-$device.ino.partitions.bin
+        wget $RELEASE/$version/$PROJECT_NAME-$device.ino.bootloader.bin
+        mv $PROJECT_NAME-$device.ino.bin $INSTALLER_PATH/$device/$version/main.bin
+        mv $PROJECT_NAME-$device.ino.partitions.bin $INSTALLER_PATH/$device/$version/partitions.bin
+        mv $PROJECT_NAME-$device.ino.bootloader.bin $INSTALLER_PATH/$device/$version/bootloader.bin
     done
 done
--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 if [ -z "$1" ]; then
     echo "Usage: sh build.sh <device>"
-    echo "tdisplay is the default device"
+    echo "esp32 is the default device"
     exit 1
 fi
-uppercase=$(echo $1 | tr '[:lower:]' '[:upper:]')
-tft_config=$(sh ./tft_build_flags.sh $1)
+device_name=$(echo $1 | tr '[:lower:]' '[:upper:]')
+tft_config_file="tft_config_$1.txt"
+if [ -f "$tft_config_file" ]; then
+    echo "Using TFT configuration file $tft_config_file."
+    user_tft_config=$(cat $tft_config_file | tr '\n' ' ' | sed -e "s/\ /\ -D/g" -e "s/-D$//")
+    tft_font="-DLOAD_GLCD=1 -DLOAD_FONT2=1 -DLOAD_FONT4=1 -DLOAD_FONT6=1 -DLOAD_FONT7=1 -DLOAD_FONT8=1 -DLOAD_GFXFF=1 -DSMOOTH_FONT=1"
+    tft_config=" -DTFT=1 -DUSER_SETUP_LOADED=1 -D${user_tft_config} ${tft_font} -DSPI_FREQUENCY=27000000 -DSPI_READ_FREQUENCY=20000000"
+fi
 arduino-cli compile \
     --build-property "build.partitions=min_spiffs" \
     --build-property "upload.maximum_size=1966080" \
-    --build-property "build.extra_flags.esp32=${tft_config} -D${uppercase}" \
+    --build-property "build.extra_flags.esp32=-D${device_name}${tft_config}" \
     --build-path build \
     --fqbn esp32:esp32:ttgo-lora32 snsd
